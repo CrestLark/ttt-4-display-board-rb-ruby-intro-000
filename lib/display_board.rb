21612b3ec3480c7013a0366ce3b3da7b0db6c54f
Stephanie Lewis <stephanie.lewis.art@gmail.com>
2021-05-19 22:19:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board =["O", "X", " ", " ", "X", " ", " ", "X", "O"]
 
 def display_board
   puts " #{board[0]} | #{board[1]} | #{board[3]} "
@@ -9,3 +9,5 @@ def display_board
   puts "-----------"
   puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
+
+display_board(board)
